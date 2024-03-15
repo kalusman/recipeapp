f136b27aa66f7ef8b5874906f6a351217ea23446
kalusman <kalusman75@gmail.com>
2024-03-15 21:11:21 +0000

    creating about page

---
+++ about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            /* padding: 20px; */
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+        }
+
+        p {
+            line-height: 1.6;
+        }
+    </style>
+</head>
+
+<body>
+<?php include("./header/header.php"); ?>
+
+    <div class="container">
+        <h1>About Me</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non diam a nibh elementum consequat. Nulla facilisi. Pellentesque vel feugiat erat. Donec at ex vitae magna gravida interdum ut sed urna. Nam pharetra nibh vel diam blandit, ut tincidunt leo malesuada. Morbi ac aliquam ex. Nulla facilisi. Integer scelerisque eros id dui consectetur, ac condimentum elit convallis. Proin at nulla vestibulum, vehicula sapien in, sagittis ligula. Vivamus vitae est nec ligula aliquam elementum. Nullam sagittis nisi ut dictum fermentum.</p>
+        <p>Morbi auctor ultricies ligula, a lacinia dui lobortis eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam pretium ligula ut odio tincidunt, ac auctor libero vehicula. In hac habitasse platea dictumst. Phasellus fermentum orci a metus aliquam, in egestas velit pulvinar. Ut nec nisi nec sem interdum tristique. Duis id felis velit. Fusce et suscipit tortor. Morbi vitae venenatis justo, eget consectetur mi.</p>
+        <p>Nulla facilisi. Morbi at tortor nec lectus ultricies fermentum. Nullam ac nunc et dui tincidunt hendrerit. Etiam luctus felis ut metus volutpat, ut venenatis turpis interdum. Duis vitae quam ac mi pharetra varius. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    </div>
+<?php include("./header/footer.php"); ?>
+
+</body>
+
+</html>
